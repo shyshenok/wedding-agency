22dba72ccc64d4ae6cc257d8a307283c8ae333b6
shyshenok <shyshenoka@gmail.com>
2017-08-01 20:01:07 +0300

    Upload  file

---
+++ js/myscript.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $("[href='#projects']").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function(){
+
+                window.location.hash = hash;
+            });
+        }
+    });
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+});
